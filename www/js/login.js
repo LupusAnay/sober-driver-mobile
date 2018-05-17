b0194758fb3e9e4ee3a7863e25e24a6bd25b5795
LupusAnay <lupusanay@gmail.com>
2018-05-17 20:33:49 +0300

    FEATURE Произведен рефакторинг кода WARNING Необходимо исправить неправильное поведение запроса на странице входа

--- www/js/login.js
+++ www/js/login.js
@@ -1,43 +1,41 @@
-//TODO Сделать рефакторинг когда
-
-$(document).ready(function () {
-    $.support.cors = true;
-    let phone_number = $("#phone_number");
-    let password = $("#password");
-    phone_number.mask("+7(000)-000-00-00");
-    $("#log_block").keyup(function () {
-        if (/[0-9+\-)(]{17}$/.test(phone_number.val()) === true && password.val() !== '') {
-            $('#reg_button').prop('disabled', false)
-        }
-        else {
-            $('#reg_button').prop('disabled', true)
+let phone_number = $("#phone_number");
+let password = $("#password");
+phone_number.mask("+7(000)-000-00-00");
+$("#log_block").keyup(function () {
+    if (/[0-9+\-)(]{17}$/.test(phone_number.val()) === true && password.val() !== '') {
+        $('#reg_button').prop('disabled', false);
+    } else {
+        $('#reg_button').prop('disabled', true);
+    }
+});
+$("#gotoregbutton").click(function () {
+    location.href = "registration.html";
+});
+$("#reg_button").click(function () {
+    let json_form = {
+        'phone': phone_number.val().replace(/[()-]+/g, ''),
+        'password': password.val()
+    };
+    $.ajax({
+        url: 'http://lupusanay.speckod.ru/login',
+        type: 'POST',
+        data: JSON.stringify(json_form),
+        success: function () {
+            location.href = 'index.html'
+        },
+        error: function (data) {
+            console.log(data);
+            navigator.notification.alert('Произошла ошибка: ' + data.responseJSON.what, null, 'Ошибка', 'Ясно');
         }
     });
-    $("#gotoregbutton").click(function () {
-        location.href = "registration.html"//регистрация
-    });
-    $("#reg_button").click(function () {
-        let phonenumber_unmasked = phone_number.val().replace(/[()-]+/g, '');
-        let jsonform = {
-            'phone': phonenumber_unmasked,
-            'password': password.val()
-        };
-        let string = JSON.stringify(jsonform);
-        console.log(JSON.stringify(jsonform));
-        let xhr = new XMLHttpRequest();
-        xhr.open("POST", "http://lupusanay.speckod.ru/login", true);
-        xhr.send(string);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState !== 4) return;
-            if (xhr.responseText === 'true') {
-                location.href = "list_of_orders.html";
-                console.log("Вы успешно вошли")
-            } else if (xhr.responseText === 'false') {
-                alert("Введены неверные данные: " + xhr.responseText);
-            } else {
-                alert("Ошибка: " + xhr.status);
-            }
-        };
-    })
 });
 
+//
+// statusCode: {
+//     200: function () {
+//         location.href = 'list_of_orders.html';
+//     },
+//     422: function (data) {
+//         navigator.notification.alert('Произошла ошибка: ' + data.responseJSON.what, null, 'Ошибка', 'Ясно')
+//     },
+// },
